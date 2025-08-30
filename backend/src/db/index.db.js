faf616facb544b125beab90eac27739b36c3da62
Nitin Reddy <nitinreddy095@gmail.com>
2025-08-30 23:11:43 +0530

    Error resolved

--- backend/src/db/index.db.js
+++ backend/src/db/index.db.js
@@ -4,7 +4,7 @@ const DB_NAME = 'Main'
 
 const connectDB = async()=>{
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log("Mongodb connected, Host:",connectionInstance.connection.host)
     } catch (error) {
         console.log("Some problem occurred",error)
